6e182934e7867bebff50cb68876ceeebb9c7a4ed
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-26 12:07:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ function Employee(name, age, jobTitle) {
 	this.jobTitle = jobTitle;
 }
 
+Object.setPrototypeOf(Employee.prototype, Person.prototype);
+Employee.prototype.constructor = Employee;
+
 Employee.prototype.jobGreet = function () {
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
 }
-
-Object.setPrototypeOf(Employee.prototype, Person.prototype);
 
 // Do not change code below this line
 window.Person = Person;
